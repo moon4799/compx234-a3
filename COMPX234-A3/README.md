52bd6377aae46f1406898743f8985f8eed4e35d0
moon4799 <h13928661910@163.com>
2025-05-07 22:57:58 +0800

    Delete COMPX234-A3 directory

--- COMPX234-A3/README.md
+++
@@ -1,6 +0,0 @@
-Stage 1: Project Skeleton
-Set up basic file structure.
-
-Implemented command-line argument parsing for client and server.
-
-No networking or data handling yet.
--- COMPX234-A3/client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 51234))
-client_socket.sendall("Hello from client".encode())
-data = client_socket.recv(1024).decode()
-print(f"Received from server: {data}")
-client_socket.close()
